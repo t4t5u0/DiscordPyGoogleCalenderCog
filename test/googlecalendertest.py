e4457cf30dbf401f947900c218671b922639260e
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-22 00:06:03 +0900

    [add] 仕様を記述

---
+++ test/googlecalendertest.py
@@ -0,0 +1,15 @@
+import datetime
+import os.path
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# 実装して確認するもの
+# 登録をコマンドライン上でする方法
+# カレンダーを登録する
+# タスクを登録する
+# タスクを確認する
+# タスクを削除する
+# カレンダーを複数登録する
