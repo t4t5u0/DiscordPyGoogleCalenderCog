24997077d32352d91670bfcebe11b1ae91a3a5e1
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-23 09:57:53 +0900

    [add] カレンダーに予定を挿入できるようにした

--- test/googlecalendertest.py
+++ test/googlecalendertest.py
@@ -22,6 +22,8 @@ from googleapiclient.discovery import build
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+CALENDAR_ID = "85th9cmqi2p3sf66kh60iqls6c@group.calendar.google.com"
+
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -51,9 +53,9 @@ def main():
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId="85th9cmqi2p3sf66kh60iqls6c@group.calendar.google.com", timeMin=now,
-                                        maxResults=10, singleEvents=True,
-                                        orderBy='startTime').execute()
+    events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
     # events_result = service.events().list(calendarId='primary', timeMin=now,
     #                                       maxResults=10, singleEvents=True,
     #                                       orderBy='startTime').execute()
@@ -65,14 +67,59 @@ def main():
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
-    page_token = None
-    while True:
-        events = service.events().list(calendarId='85th9cmqi2p3sf66kh60iqls6c@group.calendar.google.com', pageToken=page_token).execute()
-        for event in events['items']:
-            pprint(event)
-        page_token = events.get('nextPageToken')
-        if not page_token:
-            break
+    # page_token = None
+    # while True:
+    #     events = service.events().list(calendarId='85th9cmqi2p3sf66kh60iqls6c@group.calendar.google.com', pageToken=page_token).execute()
+    #     for event in events['items']:
+    #         pprint(event)
+    #     page_token = events.get('nextPageToken')
+    #     if not page_token:
+    #         break
+
+    event = {
+        'summary': 'test post',
+        'description': 'test',
+        'start': {
+            'dateTime': '2021-02-24T09:00:00',
+            'timeZone': 'Asia/Tokyo',
+        },
+        'end': {
+            'dateTime': '2021-02-24T10:00:00',
+            'timeZone': 'Asia/Tokyo',
+        },
+    }
+    # event = service.events().insert(calendarId=CALENDAR_ID, body=ex_text).execute()
+    # event = {
+    #     'summary': 'Google I/O 2015',
+    #     'location': '800 Howard St., San Francisco, CA 94103',
+    #     'description': 'A chance to hear more about Google\'s developer products.',
+    #     'start': {
+    #         'dateTime': '2015-05-28T09:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #     },
+    #     'end': {
+    #         'dateTime': '2015-05-28T17:00:00-07:00',
+    #         'timeZone': 'America/Los_Angeles',
+    #     },
+    #     'recurrence': [
+    #         'RRULE:FREQ=DAILY;COUNT=2'
+    #     ],
+    #     'attendees': [
+    #         {'email': 'lpage@example.com'},
+    #         {'email': 'sbrin@example.com'},
+    #     ],
+    #     'reminders': {
+    #         'useDefault': False,
+    #         'overrides': [
+    #             {'method': 'email', 'minutes': 24 * 60},
+    #             {'method': 'popup', 'minutes': 10},
+    #         ],
+    #     },
+    # }
+
+    event = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
+
 
 if __name__ == '__main__':
     main()
